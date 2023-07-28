29c0c72822c45e1a80b6981da42fb687a618c5eb
jcppc <jcaldeira@novaims.unl.pt>
2023-07-28 19:29:27 +0100

    Module Setup

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ setup(
         name="aistockpicker", 
         version="1.0.0",
         description="AIStockPicker",
-        url="https://github.com/jcppc/AIStockPicker", 
+        url="https://github.com/pdrleal/AIStockPicker", 
         author="Pedro Pereira & João Caldeira", 
         author_email="20220684@novaims.unl.pt",  
         packages=find_packages()
 )
-
