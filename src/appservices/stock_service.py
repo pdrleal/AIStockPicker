b5a2e6ba6486dbca1d0008810af06a5004a5c53b
pdrleal <pdrlealpereira1@gmail.com>
2023-10-01 18:00:32 +0100

    Development of different cleaning steps.

--- src/appservices/stock_service.py
+++ src/appservices/stock_service.py
@@ -14,23 +14,105 @@ class StockService(IStockService):
         self.stock_repo = stock_repo
         self.stocks_indices = self.constants_repo.get_stocks_indices()
 
+
+
     def clean_landing_data(self):
-        landing_stock_prices = self.stock_repo.get_landing_stock_prices()
 
-        df_list = []  # Initialize an empty list to store DataFrames
-        for index, row in landing_stock_prices.iterrows():
-            _, time_series_values = list(row["content"].items())[1]
-            df_list.append(pd.DataFrame(time_series_values).T)
+        def clean_stock_prices():
+            landing_stock_prices = self.stock_repo.get_landing_stock_prices()
 
-        clean_stock_prices = pd.concat(df_list)
-        clean_stock_prices.columns = ["date", "open", "high", "low", "close", "volume"]
+            df_list = []  # Initialize an empty list to store DataFrames
+            for index, row in landing_stock_prices.iterrows():
+                # create dataframe with stock values
+                _, time_series_values = list(row["content"].items())[1]
+                df_temp = pd.DataFrame(time_series_values).T
+
+                # add stock index to dataframe
+                stock_index_column = [row["stock_indice"]]*len(time_series_values)
+                df_temp.insert(loc=0, column='stock_index', value=stock_index_column)
+                df_temp["stock_index"] = stock_index_column
+
+                df_list.append(df_temp)
+
+            clean_df = pd.concat(df_list)
+            clean_df.columns = ["stock_index", "open", "high", "low", "close", "volume"]
+            clean_df.reset_index(inplace=True, drop=False,names="date")
+            clean_df.rename(columns={"index": "date"}, inplace=True)
+            return clean_df
+
+        def clean_news_sentiments():
+            landing_news_sentiments = self.stock_repo.get_landing_news_sentiments()
+
+            data = []  # Initialize an empty list to store rows
+            for index, row in landing_news_sentiments.iterrows():
+                # create dataframe with stock values
+                score = 0
+                for article in row["content"]["feed"]:
+                    for ticker_sentiment in article["ticker_sentiment"]:
+                        if ticker_sentiment["ticker"] == row["stock_indice"]:
+                            score += float(ticker_sentiment["ticker_sentiment_score"])
+                avg_score = score/len(row["content"]["feed"])
+
+                data.append({"date": pd.to_datetime(row["date"]).date(), "stock_index": row["stock_indice"], "news_sentiment": avg_score})
+
+            clean_df = pd.DataFrame(data)
+            return clean_df
+
+        def clean_social_sentiments():
+            landing_social_sentiments = self.stock_repo.get_landing_social_sentiments()
+
+            data_reddit = []  # Initialize an empty list to store reddit rows
+            data_twitter = []  # Initialize an empty list to store twitter rows
+            for index, row in landing_social_sentiments.iterrows():
+                daily_reddit = {}
+                daily_twitter = {}
+
+                # create dataframe with stock values
+                for reddit_sentiment in row["content"]["reddit"]:
+                    date = pd.to_datetime(reddit_sentiment["atTime"], format='%Y-%m-%d %H:%M:%S').date()
+                    # Initialize a list for the date if it doesn't exist in daily_info
+                    if date not in daily_reddit:
+                        daily_reddit[date] = []
+                    # Append the Reddit sentiment score to the list
+                    daily_reddit[date].append(reddit_sentiment["score"])
+
+                for twitter_sentiment in row["content"]["twitter"]:
+                    date = pd.to_datetime(twitter_sentiment["atTime"], format='%Y-%m-%d %H:%M:%S').date()
+                    # Initialize a list for the date if it doesn't exist in daily_info
+                    if date not in daily_twitter:
+                        daily_twitter[date] = []
+                    # Append the Reddit sentiment score to the list
+                    daily_twitter[date].append(twitter_sentiment["score"])
+
+                # Calculate the average score for each date and create dictionaries
+                for date, scores in daily_reddit.items():
+                    avg_score = sum(scores) / len(scores)
+                    result_dict = {"date": date, "stock_index": row["stock_indice"], "reddit_sentiment": avg_score}
+                    data_reddit.append(result_dict)
+                for date, scores in daily_twitter.items():
+                    avg_score = sum(scores) / len(scores)
+                    result_dict = {"date": date, "stock_index": row["stock_indice"], "twitter_sentiment": avg_score}
+                    data_twitter.append(result_dict)
+
+            # Create DataFrames from the lists
+            reddit_df = pd.DataFrame(data_reddit)
+            twitter_df = pd.DataFrame(data_twitter)
+
+            # Merge the DataFrames on 'date' and 'stock_index'
+            clean_df = pd.merge(reddit_df, twitter_df, on=['date', 'stock_index'], how='outer')
+
+            return clean_df
+
+        #clean_stock_prices()
+        clean_news_sentiments()
+        #clean_social_sentiments()
 
         #landing_news_sentiments = self.stock_repo.get_landing_news_sentiments()
         #landing_social_sentiments = self.stock_repo.get_landing_social_sentiments()
 
 
 
-        return raw_dataframe
+        return None
 
     def refresh_data(self):
         #retrieve stock indices from constants_service
