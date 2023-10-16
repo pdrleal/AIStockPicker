5bf2986e22092f9a26da4179269c5ca591e893ad
pdrleal <pdrlealpereira1@gmail.com>
2023-10-16 21:50:37 +0100

    Adding create_triggers.sql

---
+++ src/utils/create_triggers.sql
@@ -0,0 +1,41 @@
+use AIStockPicker;
+
+
+-- Check if the trigger exists and drop it if it does
+DROP TRIGGER IF EXISTS update_last_date_news_sentiment;
+DROP TRIGGER IF EXISTS update_last_date_stock_values;
+DROP TRIGGER IF EXISTS update_last_date_social_sentiment;
+
+DELIMITER //
+
+-- Trigger for LANDING_NEWS_SENTIMENT
+CREATE TRIGGER update_last_date_news_sentiment
+AFTER INSERT ON LANDING_NEWS_SENTIMENT
+FOR EACH ROW
+BEGIN
+    UPDATE VARIABLES
+    SET LastUpdateDate = CONVERT_TZ(NOW(), 'SYSTEM', 'Europe/Lisbon');
+END;
+//
+
+-- Trigger for LANDING_STOCK_VALUES
+CREATE TRIGGER update_last_date_stock_values
+AFTER INSERT ON LANDING_STOCK_VALUES
+FOR EACH ROW
+BEGIN
+    UPDATE VARIABLES
+    SET LastUpdateDate = CONVERT_TZ(NOW(), 'SYSTEM', 'Europe/Lisbon');
+END;
+//
+
+-- Trigger for LANDING_SOCIAL_SENTIMENT
+CREATE TRIGGER update_last_date_social_sentiment
+AFTER INSERT ON LANDING_SOCIAL_SENTIMENT
+FOR EACH ROW
+BEGIN
+    UPDATE VARIABLES
+    SET LastUpdateDate = CONVERT_TZ(NOW(), 'SYSTEM', 'Europe/Lisbon');
+END;
+//
+
+DELIMITER ;
